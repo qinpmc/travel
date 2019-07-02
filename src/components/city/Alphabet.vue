9caaa74f7699921dec9e132dd51d532ed87028d3
qinpmc <850823973@qq.com>
2019-07-02 22:10:05 +0800

    修复移动

--- src/components/city/Alphabet.vue
+++ src/components/city/Alphabet.vue
@@ -1,17 +1,51 @@
 <template>
     <ul class="list" >
-        <li class="item" v-for="(value,key) of cities" :key="value.id"
+        <li class="item" v-for="(value) of letters" :key="value"  :ref="value"
             @click = "handleLetterClick"
-        >{{key}}</li>
+            @touchstart = "handleTouchStart"
+            @touchmove = "handleTouchMove"
+            @touchend = "handleTouchEnd" 
+        >{{value}}</li>
     </ul>
 </template>
 <script>
 export default {
     name:"Alphabet",
     props:["cities"],
+    data(){
+        return{
+            touchStatus:false
+        }
+    },
+    computed:{
+        letters(){
+            const letters = [];
+            for(let i in this.cities){
+                letters.push(i);
+            }
+            return letters;
+        }
+    },
     methods:{
         handleLetterClick(e){
             this.$emit("letterChange",e.target.innerText)
+        },
+        handleTouchStart(e){
+            this.touchStatus = true;
+        },
+        handleTouchMove(e){
+            if(this.touchStatus){
+                let startY = this.$refs["A"][0].offsetTop; //67
+                let touchY = e.touches[0].clientY -79;
+                let index = Math.floor((touchY - startY)/20);
+                if(index>=0 && index<this.letters.length){
+                    this.$emit("letterChange",this.letters[index]);
+                }
+                
+            }
+        },
+        handleTouchEnd(e){
+            this.touchStatus = false;
         }
     }
 }
