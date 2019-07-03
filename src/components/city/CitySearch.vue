656478237b23550cfa8b294abf7a83133a9fbfb0
qinpmc <850823973@qq.com>
2019-07-03 22:39:37 +0800

    add search

--- src/components/city/CitySearch.vue
+++ src/components/city/CitySearch.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="search">
-       <input type="text" name="" id="" placeholder="请输入城市名称" class="search-input">
+    <div>
+        <div class="search">
+            <input type="text" name="" id="" placeholder="请输入城市名称" class="search-input">
+        </div>
+        <div class="search-content">
+
+        </div>
     </div>
+
 </template>
 <script>
 export default {
@@ -24,5 +30,15 @@ export default {
             text-align:center
             border radius 0.06rem
             color:#666
+    .search-content
+        z-index:1
+        overflow: hidden
+        position:absolute
+        top:1.58rem
+        left:0
+        right:0
+        bottom:0
+        background:green
+
 </style>
  
